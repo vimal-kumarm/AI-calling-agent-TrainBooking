f17bb7e1735373a3870dde0e37913b1deb085d0c
VIMALKUMAR <vimalkumarm196@gmail.com>
2026-05-24 16:06:46 +0530

    Remove sensitive data from config.py
    Updated configuration file to remove sensitive credentials and added placeholders.

--- config.py
+++ config.py
@@ -3,16 +3,16 @@
 
 # ElevenLabs API Key for text-to-speech
 # Get from: https://elevenlabs.io/
-ELEVENLABS_API_KEY = "sk_2edc0358e3702c75f32c22ea25bb2cb5aed0a70f4b7795ac"
+ELEVENLABS_API_KEY = ""
 
 # Twilio Configuration
 # Get from: https://www.twilio.com/
-TWILIO_ACCOUNT_SID = "ACf6f122a7b070ef9839f2432adb91c28f"
-TWILIO_AUTH_TOKEN = "2ba2d0fd417bd68836427f52942baf50"
-TWILIO_FROM_NUMBER = "+19068286642"  # Your Twilio phone number
+TWILIO_ACCOUNT_SID = ""
+TWILIO_AUTH_TOKEN = ""
+TWILIO_FROM_NUMBER = ""  # Your Twilio phone number
 
 # Phone number to call
-TO_NUMBER = "+918148429945"
+TO_NUMBER = ""
  # The number you want to call (your number)
 
 # Default message to speak
@@ -20,5 +20,5 @@ DEFAULT_MESSAGE = "Hello! This is your AI assistant calling. I hope you're havin
 
 # Gmail Configuration for sending emails
 # Get App Password from: https://myaccount.google.com/apppasswords
-GMAIL_USER = "vimalkumarm196@gmail.com"  # Your Gmail address
-GMAIL_APP_PASSWORD = "Vimalkumar@123"  # Your Gmail App Password (16 characters)
+GMAIL_USER = "@gmail.com"  # Your Gmail address
+GMAIL_APP_PASSWORD = "@123"  # Your Gmail App Password (16 characters)
